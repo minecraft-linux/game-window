b8530e88b5a897c85ce8fe73956c5728f0b82a55
MCMrARM <mcmrarm@gmail.com>
2018-11-11 21:24:27 +0100

    Add support for EGLUT x11 WM_CLASS

--- src/window_manager_eglut.cpp
+++ src/window_manager_eglut.cpp
@@ -2,8 +2,15 @@
 #include "window_eglut.h"
 #include <EGL/egl.h>
 #include <eglut.h>
+#include <eglut_x11.h>
+#include <unistd.h>
+#include <linux/limits.h>
+#include <libgen.h>
 
 EGLUTWindowManager::EGLUTWindowManager() {
+    char buf[PATH_MAX];
+    readlink("/proc/self/exe", buf, sizeof(buf) - 1);
+    eglutInitX11ClassInstanceName(basename(buf));
     eglutInit(0, nullptr); // the args aren't really required and are troublesome to pass with this system
 }
 
