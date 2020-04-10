ad06ceb69069faba7d6e324e0541830f8123f294
MCMrARM <mcmrarm@gmail.com>
2020-04-10 19:18:50 +0200

    Delete eglut/linux-gamepad integration

--- BuildSettings.cmake
+++ BuildSettings.cmake
@@ -1,6 +1,3 @@
-set(GAMEWINDOW_SYSTEM_DEFAULT EGLUT)
-if (APPLE)
-    set(GAMEWINDOW_SYSTEM_DEFAULT GLFW)
-endif()
+set(GAMEWINDOW_SYSTEM_DEFAULT GLFW)
 
-set(GAMEWINDOW_SYSTEM ${GAMEWINDOW_SYSTEM_DEFAULT} CACHE STRING "The implementation to use for windows - EGLUT or GLFW")
+set(GAMEWINDOW_SYSTEM ${GAMEWINDOW_SYSTEM_DEFAULT} CACHE STRING "The implementation to use for windows - only GLFW is currently supported")
